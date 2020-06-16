fb5672f17e40b650c114f2a313837fdd5fed61e2
takeshidev <59844178+takeshidev@users.noreply.github.com>
2020-06-15 20:53:34 -0400

    Commit inicial

---
+++ js/login.js
@@ -0,0 +1,38 @@
+function login() {
+    var user = document.getElementById("usuario").value;
+    window.localStorage.setItem('user', user);
+    // var val = JSON.parse(window.localStorage.getItem('user'));
+    console.log(user);
+    document.getElementById("nomuser").innerHTML = user;
+    document.getElementById("nohayusuario").hidden = true;
+    document.getElementById("sihayusuario").hidden = true;
+}
+
+
+
+$(function() { //al cargar el documento...
+
+    verificarusuario();
+
+});
+
+
+function verificarusuario() {
+    console.log("Verificando usuario")
+    var user2 = window.localStorage.getItem('user');
+    console.log(user2)
+    if (user2 !== null) {
+        document.getElementById("nomuser").innerHTML = user2;
+        document.getElementById("nohayusuario").hidden = true;
+        document.getElementById("sihayusuario").hidden = false;
+    } else {
+        document.getElementById("sihayusuario").hidden = true;
+    }
+}
+
+function cerrarsesion() {
+
+    window.localStorage.clear();
+    console.log("Cerrando sesion");
+    location.reload();
+}
